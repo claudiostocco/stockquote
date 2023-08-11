d93cbdf6a1621165cae9977a42eb500d46515df6
claudiostocco <claudiostocco@gmail.com>
2023-08-11 07:59:43 -0300

    First project

---
+++ firstproj/main.py
@@ -0,0 +1,6 @@
+from pandas_datareader import data as web
+import pandas as pd
+import matplotlib.pyplot as plt
+
+quote = web.DataReader('^BVSP', data_source='yahoo', start="08-11-2023", end="08-11-2023")
+display(quote)
