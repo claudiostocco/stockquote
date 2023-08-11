f0a01c300dfac5c6701cb133195b5530503cae1e
claudiostocco <claudiostocco@gmail.com>
2023-08-11 17:52:39 -0300

    Test YahooDailyReader class

--- firstproj/main.py
+++ firstproj/main.py
@@ -2,5 +2,6 @@ from pandas_datareader import data as web
 import pandas as pd
 import matplotlib.pyplot as plt
 
-quote = web.DataReader('^BVSP', data_source='yahoo', start="08-11-2023", end="08-11-2023")
-display(quote)
+# quote = web.DataReader('^BVSP', data_source='yahoo', start='08-11-2023', end='08-11-2023')
+quote = web.YahooDailyReader('^BVSP', start='08-11-2023', end='08-11-2023')
+print(quote.read())
