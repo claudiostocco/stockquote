81f288b0eeaffc63783cf519b992fe8f49d24ec8
claudiostocco <claudiostocco@gmail.com>
2023-08-11 18:08:19 -0300

    Second project

--- secondproj/main.py
+++ secondproj/main.py
@@ -1,10 +1,11 @@
 import yfinance as yf
 from datetime import datetime
 
-# ticker = yf.Ticker('MGLU3.SA')
+ticker = yf.Ticker('MGLU3.SA')
 # print(ticker.get_actions())
 # print(ticker.info)
+data_ticker = ticker.history()
 
-end_data = datetime.now().strftime('%Y-%m-%d')
-data_ticker = yf.download("VALE", start="2023-08-01", end=end_data, progress=False)
+# end_data = datetime.now().strftime('%Y-%m-%d')
+# data_ticker = yf.download("VALE", start="2023-08-01", end=end_data, progress=False)
 print(data_ticker)
